d729098c34ed009cd17418be55e0fa0b4e2ea097
Behnam Hamidi <eslp91@hotmail.com>
2024-03-13 14:33:34 +0330

    logout route

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,17 +4,9 @@ import "./dashboard.css";
 const Dashboard = () => {
   return (
     <div className="dashboard">
-      <h1>Welcome To Your Admin Dashboard</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet unde
-        officiis deserunt quisquam eius mollitia quas voluptatum ut ex similique
-        labore expedita, facere reprehenderit sequi ratione neque, totam nulla
-        hic!
-      </p>
-
-      <p>Name:</p>
-      <p>Username:</p>
-      <p>Email:</p>
+      <h1>Welcome To Your Admin Dashboard: $User</h1>
+      <p>Click here to go to your dashboard</p>
+      <button className="logoutBtn">Logout</button>
     </div>
   );
 };
