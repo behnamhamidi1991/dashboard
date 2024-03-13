d7242c80f6ded4a85fc79c359f8b0c90835ae55c
Behnam Hamidi <eslp91@hotmail.com>
2024-03-13 14:44:05 +0330

    Logout route is done

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,32 @@
+"use client";
 import React from "react";
 import "./dashboard.css";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const Dashboard = () => {
+  const router = useRouter();
+  const [data, setData] = React.useState("nothing");
+
+  const onLogout = async () => {
+    try {
+      await axios.get("/api/users/logout");
+      toast.success("You logged out!");
+      router.push("/");
+    } catch (error: any) {
+      console.log(error.message);
+      toast.error(error.message);
+    }
+  };
+
   return (
     <div className="dashboard">
       <h1>Welcome To Your Admin Dashboard: $User</h1>
       <p>Click here to go to your dashboard</p>
-      <button className="logoutBtn">Logout</button>
+      <button className="logoutBtn" onClick={onLogout}>
+        Logout
+      </button>
     </div>
   );
 };
