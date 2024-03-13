2c392d22e353d3f1509c34aed0b0571d33633bfd
Behnam Hamidi <eslp91@hotmail.com>
2024-03-13 13:12:46 +0330

    login route

---
+++ src/app/api/users/login/route.ts
@@ -0,0 +1,52 @@
+import { connectDB } from "@/database/dbConfig";
+import User from "@/models/userModel";
+import { NextResponse, NextRequest } from "next/server";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { toast } from "react-toastify";
+
+connectDB();
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { email, password } = reqBody;
+    console.log(reqBody);
+
+    // Check if the user exists
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return NextResponse.json(
+        {
+          error: "User does not exists",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    // Create token data
+    const tokenData = {
+      id: user._id,
+      username: user.email,
+    };
+
+    // Create token
+    const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET!, {
+      expiresIn: "1d",
+    });
+
+    const response = NextResponse.json({
+      message: "Login successful",
+      success: true,
+    });
+
+    response.cookies.set("token", token, { httpOnly: true, path: "/" });
+
+    return response;
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
