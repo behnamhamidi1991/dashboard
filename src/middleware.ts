4088c84f2ddf5ea3d58af222a676d9628c4be125
Behnam Hamidi <eslp91@hotmail.com>
2024-03-15 10:38:05 +0330

    textarea

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ export function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
 
   const isPublicPath = path === "/login" || path === "/signup";
-  const isPrivate = path === "/dashboard";
+  const isPrivate = path === "/dashboard" || path === "/blog";
 
   const token = request.cookies.get("token")?.value || "";
 
@@ -19,5 +19,5 @@ export function middleware(request: NextRequest) {
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ["/dashboard", "/login", "/signup"],
+  matcher: ["/dashboard", "/login", "/signup", "/blog"],
 };
